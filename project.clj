a797813ba00c4634a32ce82d46c34d95310f9f5b
Joe Wass <jwass@crossref.org>
2018-03-29 13:10:00 +0100

    Add Safai compatibiliy.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject event-live-demo "0.1.16"
+(defproject event-live-demo "0.1.17"
   :description "Event Data Live Demo"
   :url "http://eventdata.crossref.org/"
   :license {:name "MIT License"
