1f18f4ffba66a9235fd086252b445acb8ce9f0f8
Joe Wass <jwass@crossref.org>
2018-03-29 12:20:10 +0100

    Update sound envelope.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject event-live-demo "0.1.15"
+(defproject event-live-demo "0.1.16"
   :description "Event Data Live Demo"
   :url "http://eventdata.crossref.org/"
   :license {:name "MIT License"
